b27f054f7c54002d19ec7c7d038ee1cf2d318246
BonkeG <bonkegcobo291195@gmail.com>
2021-09-07 12:41:25 +0200

    Add mobile menu

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 window.onload= ()=> {
 
-  const hamburger= document.querySelector('#hamburger');
+    const hamburger= document.querySelector('#hamburger');
+  
+    hamburger.addEventListener('click', function(event){
+      const my_show=document.querySelector('.mob-menu');
+      my_show.classList.toggle('show');
+      
+  });
 
-  hamburger.addEventListener('click', function(event){
-    const my_show=document.querySelector('.mob-menu');
-    my_show.classList.add('.showed');
-    
-})
+    const close_btn=document.querySelector('.cancel_btn');
+    close_btn.addEventListener('click', function(event){
+        const my_show=document.querySelector('.mob-menu');
+        my_show.classList.toggle('show');
+    });
 
-
-
-
-}
+  }
