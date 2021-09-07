2d6c953ab1312d03348bb5f5ac0dff73def79f52
BonkeG <bonkegcobo291195@gmail.com>
2021-09-07 17:22:06 +0200

    Fix JS linters

--- main.js
+++ main.js
@@ -1,31 +1,27 @@
-window.onload= ()=> {
+window.onload = () => {
 
-    const hamburger= document.querySelector('#hamburger');
-    hamburger.addEventListener('click', function(event){
-      const my_show=document.querySelector('.mob-menu');
-      const the_head=document.querySelector('.HEAD');
-      the_head.classList.toggle('make_blur');
-      my_show.classList.toggle('show');
+  const hamburger= document.querySelector('#hamburger');
+  hamburger.addEventListener('click', function(event){
+  const myShow = document.querySelector('.mob-menu');
+  const theHead = document.querySelector('.HEAD');
+  theHead.classList.toggle('make_blur');
+  myShow.classList.toggle('show');
       
   });
 
-    const close_btn=document.querySelector('.cancel_btn');
-    close_btn.addEventListener('click', function(event){
-        const my_show=document.querySelector('.mob-menu');
-        const the_head=document.querySelector('.HEAD');
-        the_head.classList.toggle('make_blur');
-        my_show.classList.toggle('show');
-    });
+    const closeBtn=document.querySelector('.cancel_btn');
+    closeBtn.addEventListener('click', function(event) {
+    const myShow = document.querySelector('.mob-menu');
+    const theHead = document.querySelector('.HEAD');
+    theHead.classList.toggle('make_blur');
+    myShow.classList.toggle('show');
+  });
 
-    const mobitems = document.querySelector ('.mob-items');
-    
-    mobitems.addEventListener('click', function(event){
-      const my_show = document.querySelector('.mob-menu');
-      const the_head=document.querySelector('.HEAD');
-      the_head.classList.toggle('make_blur');
-      my_show.classList.toggle('show');
-    });
-
-   
-
-  }
+    const mobItems = document.querySelector ('.mob-items');
+    mobItems.addEventListener('click', function(event) {
+    const myShow = document.querySelector('.mob-menu');
+    const theHead = document.querySelector('.HEAD');
+    theHead.classList.toggle('make_blur');
+    myShow.classList.toggle('show');
+  });
+};
