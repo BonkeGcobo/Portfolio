6c905b4183d7f47e57bbf760fe37b048bfa3f110
BonkeG <bonkegcobo291195@gmail.com>
2021-09-07 13:59:52 +0200

    Make menu for mobile

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 window.onload= ()=> {
 
     const hamburger= document.querySelector('#hamburger');
-  
     hamburger.addEventListener('click', function(event){
       const my_show=document.querySelector('.mob-menu');
+      const the_head=document.querySelector('.HEAD');
+      the_head.classList.toggle('make_blur');
       my_show.classList.toggle('show');
       
   });
