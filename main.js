b502ae9519e356db78d01958e884725d92af2c63
BonkeG <bonkegcobo291195@gmail.com>
2021-09-09 22:52:31 +0200

    Fix desktop Pop up

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ window.onload = () => {
   });
 
     
-/*const store = {
+const store = {
 
   detail1: {
     name: 'Tonic',
@@ -45,30 +45,44 @@ window.onload = () => {
   },
 
   detail2: {
-    name: 'Multi-Post Stories',
+    name: 'Tonic',
+    name_desk: 'Facebook 360',
     img: 'images/Project_2.svg',
-
+    img_desk:'images/desk_2.svg',
     companyName: 'Canopy',
     job: 'Full Stack Dev',
     year: 2015,
+    companyNameDesk: 'Canopy',
+    jobDesk: 'Full Stack Dev',
+    yearDesk: 2015,
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing.',
     tags: ['html', 'css', 'javaScript'],
   },
 
   detail3: {
     name: 'Tonic',
+    name_desk: 'Facebook 360',
     img: 'images/Project_3.svg',
+    img_desk: 'images/desk_3.svg',
     companyName: 'Canopy',
     job: 'Back End Dev',
     year: 2015,
+    companyNameDesk: 'Canopy',
+    jobDesk: 'Full Stack Dev',
+    yearDesk: 2015,
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing.',
     tags: ['html','css', 'javaScript'],
   },
 
   detail4: {
-    name: 'Multi-Post Stories',
+    name: 'Tonic',
+    name_desk: 'Uber Navigation',
     img: 'images/Project_4.svg',
+    img_desk: 'images/Project_4_desktop.svg',
     companyName: 'Canopy',
+    companyNameDesk: 'Uber',
+    jobDesk: 'Lead Developer',
+    yearDesk: 2018,
     job: 'Back End Dev',
     year: 2018,
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate. ',
@@ -80,7 +94,7 @@ window.onload = () => {
 const name=document.querySelector('.nameOfProject');
 const name_desk=document.querySelector('.nameOfProject_desk'); /*Calling desktop project*/ 
 
-/*const companyName=document.getElementById('company');
+const companyName=document.getElementById('company');
 const companyNameDesk=document.getElementById('company_Desk');
 
 const job=document.getElementById('job');
@@ -93,7 +107,7 @@ const image=document.getElementById('img');
 const imageDesk=document.getElementById('imgDesk');
 
 const text=document.getElementById('pop_Text');
-const Desktext=document.getElementById('pop_text_desk');
+const Desktext=document.getElementById('pop_Text_desk');
 
 const btn1 = document.querySelector('.btnProj1');
 const btn2 = document.querySelector('.btnProj2');
@@ -116,6 +130,7 @@ function media(size) {
 const span = document.getElementsByClassName('close')[0];
 const span_desk = document.getElementsByClassName('close_desk')[0];
 const tags = document.getElementById('tags');
+const tags_desk = document.getElementById('tags_desk');
 
 
 function changePopup(order) {
@@ -129,6 +144,7 @@ function changePopup(order) {
   console.log(detail.name);
 
   companyName.innerHTML = detail.companyName;
+  console.log(companyName.innerHTML);
   companyNameDesk.innerHTML=detail.companyNameDesk;
 
   job.innerHTML = detail.job;
@@ -142,7 +158,9 @@ function changePopup(order) {
 
   const tagsLength = detail.tags.length;
   const tagDiv = document.querySelector('.tag_list');
+  const tagDivDesk = document.querySelector('.tag_list_desk');
   tagDiv.innerHTML="";
+  tagDivDesk.innerHTML="";
   
   
 
@@ -152,6 +170,7 @@ function changePopup(order) {
     element.textContent = detail.tags[i];
     console.log(element.textContent);
     tagDiv.appendChild(element);
+    tagDivDesk.appendChild(element)
     element.style.display = 'inline';
     element.style.alignItems = 'center';
     element.style.borderRadius = '8px';
@@ -198,116 +217,4 @@ btn4.onclick = function bar() {
   modal_mob.style.display = 'block';
   changePopup(4);
 };
-
-
-
 };
-*/
-const projectContent = [
-  {
-    Image: 'images/Project_1.svg',
-    description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita',
-    name: 'Multi-Post Stories',
-    technologies: ['Ruby on rails', 'Bootstrap', 'JavaScript'],
-    links: [
-      'https://tufoinnkuo10.github.io/Personal-Portfolio',
-      'https://github.com/tufoinnkuo10/Personal-Portfolio',
-    ],
-  },
-  {
-    Image: './image/prof-print.png',
-    description:
-      ' A daily selection of privatel personalized reads; no accountssign-ups required. Has been the industry standard',
-    name: 'card 2',
-    technologies: ['Ruby on rails', 'css', 'JavaScript'],
-    links: [
-      'https://tufoinnkuo10.github.io/Personal-Portfolio',
-      'https://github.com/tufoinnkuo10/Personal-Portfolio',
-    ],
-  },
-  {
-    Image: './image/dash-data.png',
-    description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita',
-    name: 'card 3',
-    technologies: ['Ruby on rails', 'css', 'JavaScript'],
-    links: [
-      'https://tufoinnkuo10.github.io/Personal-Portfolio',
-      'https://github.com/tufoinnkuo10/Personal-Portfolio',
-    ],
-  },
-  {
-    Image: './image/dash-data.png',
-    description:
-      'A daily selection of privatel personalized reads; no accountssign-ups required. Has been the industry standard',
-    name: 'card 4',
-    technologies: ['Ruby on rails', 'css', 'JavaScript'],
-    links: [
-      'https://tufoinnkuo10.github.io/Personal-Portfolio',
-      'https://github.com/tufoinnkuo10/Personal-Portfolio',
-    ],
-  },
-  {
-    Image: './image/websitepro.png',
-    description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita',
-    name: 'card 5',
-    technologies: ['Python', 'React', 'bootstrap'],
-    links: [
-      'https://tufoinnkuo10.github.io/Personal-Portfolio',
-      'https://github.com/tufoinnkuo10/Personal-Portfolio',
-    ],
-  },
-  {
-    Image: './image/websitepro.png',
-    description:
-      'A daily selection of privatel personalized reads; no accountssign-ups required. Has been the industry standard',
-    name: 'card 6',
-    technologies: ['Ruby on rails', 'css', 'JavaScript'],
-    links: [
-      'https://tufoinnkuo10.github.io/Personal-Portfolio',
-      'https://github.com/tufoinnkuo10/Personal-Portfolio',
-    ],
-  },
-];
-
-const buttons = document.querySelectorAll('.cproject');
-buttons.forEach((btn, i) => {
-  btn.addEventListener('click', () => {
-    const project = projectContent[i];
-
-    const popup = document.querySelector('.mod-pop');
-    popup.style.display = 'flex';
-
-    // get title of popup
-    const popupTitle = popup.querySelector('.card-title');
-    const popupImage = popup.querySelector('.mod-image');
-    const popupTech = popup.querySelector('.mod-btn');
-    const popupDesc = popup.querySelector('.mod-text');
-
-    popupTitle.textContent = project.name;
-    popupImage.src = project.Image;
-
-    popupTech.querySelectorAll('button').forEach((tech, j) => {
-      tech.textContent = project.technologies[j];
-    });
-
-    popupDesc.textContent = project.description;
-
-    const aTags = popup.querySelectorAll('.btnz');
-    const [liveLink, sourceLink] = project.links;
-
-    aTags[0].href = liveLink;
-    aTags[1].href = sourceLink;
-  });
-});
-
-const closeElement = document.querySelector('.mob-close');
-closeElement.addEventListener('click', () => {
-  const popup = document.querySelector('.mod-pop');
-  popup.style.display = 'none';
-  console.log (closeElement);
-});
-
-
